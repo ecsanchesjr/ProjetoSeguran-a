6092221903e174409371282fa3509e38eff7b8ad
Elcio Cezario Sanches Junior <ecsanchesjr@hotmail.com>
2017-09-18 00:56:58 -0300

    Validação de NewEntry

--- addentry.cpp
+++ addentry.cpp
@@ -10,12 +10,12 @@ addEntry::addEntry(QWidget *parent) : QFrame(),
     ui->setupUi(this);
     principalref = (Principal *)parent;
     //Ajuste da tela
-    int width=this->frameGeometry().width();
-    int height=this->frameGeometry().height();
+    int width = this->frameGeometry().width();
+    int height = this->frameGeometry().height();
     QDesktopWidget btt;
-    int screenWidth=btt.screen()->width();
-    int screenHeight=btt.screen()->height();
-    this->setGeometry((screenWidth/2) -(width/2),(screenHeight/2)-(height/2),width,height);
+    int screenWidth = btt.screen()->width();
+    int screenHeight = btt.screen()->height();
+    this->setGeometry((screenWidth / 2) - (width / 2), (screenHeight / 2) - (height / 2), width, height);
 }
 
 addEntry::~addEntry()
@@ -25,11 +25,18 @@ addEntry::~addEntry()
 
 void addEntry::on_pushButton_clicked()
 {
-    passDialog *pD = new passDialog(principalref,this,
-                                    ui->lineEdit->text().toStdString(),
-                                    ui->lineEdit_2->text().toStdString(),
-                                    ui->lineEdit_3->text().toStdString());
-    pD->show();
+    if (ui->lineEdit->text().toStdString().length() != 0 && ui->lineEdit_2->text().toStdString().length() != 0 && ui->lineEdit_3->text().toStdString().length() != 0)
+    {
+        passDialog *pD = new passDialog(principalref, this,
+                                        ui->lineEdit->text().toStdString(),
+                                        ui->lineEdit_2->text().toStdString(),
+                                        ui->lineEdit_3->text().toStdString());
+        pD->show();
+    }
+    else
+    {
+        principalref->displayErrorMessage("Empty Input Field");
+    }
 }
 
 void addEntry::closeEvent(QCloseEvent *event)
@@ -37,5 +44,4 @@ void addEntry::closeEvent(QCloseEvent *event)
     event->ignore();
     this->hide();
     delete ui;
-
 }
